4ed257ef7cd21d4b87cceb7b97111a170aa4065f
Shaswatxd <117525421+LuciOPxD@users.noreply.github.com>
2026-07-03 15:24:23 +0530

    update contact info

--- src/App.jsx
+++ src/App.jsx
@@ -477,7 +477,7 @@ export default function App() {
           <div className="nav-center">
             <a href="#projects" className="nav-link">Projects</a>
             <a href="#stack" className="nav-link">Stack</a>
-            <a href="mailto:support@shaswathub.xyz" className="nav-link">Contact</a>
+            <a href="mailto:srijankumardeo777@gmail.com" className="nav-link">Contact</a>
           </div>
 
           <a
@@ -597,13 +597,7 @@ export default function App() {
               </svg>
               GitHub
             </a>
-            <a href="#" target="_blank" rel="noopener noreferrer">
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
-              </svg>
-              LinkedIn
-            </a>
-            <a href="mailto:support@shaswathub.xyz">
+            <a href="mailto:srijankumardeo777@gmail.com">
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                 <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/>
                 <polyline points="22,6 12,13 2,6"/>
